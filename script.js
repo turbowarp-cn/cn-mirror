2cc9dfc1fc4f68c9bece3b32df63462ed9f590be
-SIPC- <92251518+SIPC@users.noreply.github.com>
2023-10-21 21:33:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadApp(version, architecture) {
     console.log('正在下载应用版本：' + version + '，架构：' + architecture);
-    var url = `http://cdn.wearclub.online/TurboWarp-mirror/${architecture}/${version}.exe`
-    window.open(url, '_blank');
+    var url = `http://cdn.wearclub.online/TurboWarp-mirror/${architecture}/TurboWarp-Setup-${version}-${architecture}.exe`
+    var link = document.createElement('a'); link.href = url; link.download = `TurboWarp-Setup-${version}-${architecture}.exe`
+    link.click();
   }
-  
