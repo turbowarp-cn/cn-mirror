8cf4d3df3e1031d40f539bdcd198f6e1a0ec927e
sipc.ink <92251518+SIPC@users.noreply.github.com>
2024-01-22 22:32:40 +0800

    update

---
+++ scriptv4.js
@@ -0,0 +1,19 @@
+function downloadApp(version, architecture) {
+  console.log('正在下载应用版本：' + version + '，架构：' + architecture);
+  var url = `https://cdn.wearclub.online/TurboWarp-mirror/${architecture}/TurboWarp-Setup-${version}-${architecture}.exe`
+  var link = document.createElement('a'); link.href = url; link.download = `TurboWarp-Setup-${version}-${architecture}.exe`
+  link.click();
+}
+function downloadApp_Desk(version, architecture) {
+  console.log('正在下载应用版本：' + version + '，架构：' + architecture);
+  var url = `https://cdn.wearclub.online/TurboWarp-mirror/${architecture}/${version}.exe`
+  var link = document.createElement('a'); link.href = url; link.download = `${version}.exe`
+  link.click();
+}
+
+function downloadApp_new(version, architecture) {
+  console.log('正在下载应用版本：' + version + '，架构：' + architecture);
+  var url = `https://data.turbowarp.cn/${architecture}/TurboWarp-Setup-${version}-${architecture}.exe`
+  var link = document.createElement('a'); link.href = url; link.download = `TurboWarp-Setup-${version}-${architecture}.exe`
+  link.click();
+}
